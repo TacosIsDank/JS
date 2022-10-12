67a0a647764442b1552401a38b63a8e917e037e5
Johannes <59510166+Joonsey@users.noreply.github.com>
2022-10-13 00:51:32 +0200

    indents are nice : )

--- javascript/slider.js
+++ javascript/slider.js
@@ -1,37 +1,37 @@
 var imageCounter = 1;
 
-showDiv(imageCounter);
+  showDiv(imageCounter);
 
-function changeImage(m) {
+  function changeImage(m) {
 
-showDiv(imageCounter = imageCounter + m);
+  showDiv(imageCounter = imageCounter + m);
 
 }
 
 function showDiv(n) {
 
-var i;
+  var i;
 
-var imageArray = document.getElementsByClassName("mySlides");
+  var imageArray = document.getElementsByClassName("mySlides");
 
-if (n > imageArray.length) {
+  if (n > imageArray.length) {
 
-imageCounter = 1;
+  imageCounter = 1; 
 
 }
 
 if (n < 1) {
 
-imageCounter = imageArray.length;
+  imageCounter = imageArray.length;
 
 }
 
-for (i = 0; i < imageArray.length; i++) {
+  for (i = 0; i < imageArray.length; i++) {
 
-imageArray[i].style.display = "none";
+  imageArray[i].style.display = "none";
 
 }
 
-imageArray[imageCounter - 1].style.display = "block";
+  imageArray[imageCounter - 1].style.display = "block";
 
 }
